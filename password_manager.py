638e300ecb4b9406b2d20374905b306ed54188ff
Shubham <shubhmk17@gmail.com>
2026-08-09 15:09:48 +0530

    Implement random password generator fn

--- password_manager.py
+++ password_manager.py
@@ -1,15 +1,22 @@
 import random
 import string
 
-passwords = {}
+password = {}
 
 try:
     with open("password.txt", "r") as file:
         for line in file:
-            website, password = line.strip().split(":") #strip used to remove extra spaces and split used to defferenciate id and password
-            passwords[website] = password #passwords mai jao aur ye raha uska ID or ab password DISPLAY kar dho
+            website, pwd = line.strip().split(":") #strip used to remove extra spaces and split used to defferenciate id and password
+            password[website] = pwd #password mai jao aur ye raha uska ID or ab password DISPLAY kar dho
 
 except:
     pass    
+
+def random_password_generator():
+    chars = string.ascii_letters + string.digits + "@#&^%$%()*&/*-+"
+    password = " ".join(random.choice(chars) for _ in range (8))
+    return password
+
+
 
     
