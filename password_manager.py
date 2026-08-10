8601e10c093d2fc54b42a15adb1b5faa8c99f1f4
Shubham <shubhmk17@gmail.com>
2026-08-10 13:57:19 +0530

    Did some changes in code

--- password_manager.py
+++ password_manager.py
@@ -1,4 +1,4 @@
-import random
+import secrets
 import string
 
 password = {}
@@ -9,14 +9,16 @@ try:
             website, pwd = line.strip().split(":") #strip used to remove extra spaces and split used to defferenciate id and password
             password[website] = pwd #password mai jao aur ye raha uska ID or ab password DISPLAY kar dho
 
-except:
+except FileNotFoundError:
     pass    
 
 def random_password_generator():
-    chars = string.ascii_letters + string.digits + "@#&^%$%()*&/*-+"
-    password = " ".join(random.choice(chars) for _ in range (8))
-    return password
-
-
-
+    try:
+        length = int(input("How much length of password you need: "))
+    except ValueError:
+        print("Please enter length in number")    
+        return
     
+    chars = string.ascii_letters + string.digits + "@#&^%$%&/-+"
+    password = "".join(secrets.choice(chars) for _ in range(length))
+    return password
